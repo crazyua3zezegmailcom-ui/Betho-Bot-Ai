5650008e17d78d5cdb301d6ec7cafd7190b071c1
DevCrazyForBots <59079447-DevCrazyForBots@users.noreply.replit.com>
2026-06-16 22:41:46 +0000

    Update

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ import chalk from "chalk"
 import { fileURLToPath } from "url"
 import fs from "fs"
 // ملف الاعدادات يلزمك التعديل في بعض المناطق
-global.botNumber = "201116571308"
+global.botNumber = "201039981389"
 
 // عدل في محل owenar لي كي تحط نفسك مالك للبوت 
 global.owner = ["584261208048", "201214057674", "201201756710"]
